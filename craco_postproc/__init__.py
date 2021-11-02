b8c1090ea8ccaaa58042957b16c43f1f54694510
d-r-scott <danica.scott@postgrad.curtin.edu.au>
2021-11-02 11:16:04 +0800

    :tada: Initial commit

---
+++ craco_postproc/__init__.py
@@ -0,0 +1,15 @@
+# type: ignore[attr-defined]
+"""Post-processing pipelines for CRACO"""
+
+import sys
+from importlib import metadata as importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
