d9f6828a07313a321236cbdafab6abbf9ca1c29b
d-r-scott <danica.scott@postgrad.curtin.edu.au>
2021-11-09 15:30:29 +0800

    Tidy up and document find_vcraft

--- craco_postproc/localise/processTimeStep.py
+++ craco_postproc/localise/processTimeStep.py
@@ -8,8 +8,8 @@ import sys
 
 
 def _main():
+    # Parse and verify command line arguments
     args = get_args()
-    verify_args(args)
 
     # A few useful paths
     datadir = os.path.abspath(args.timestep)
@@ -26,6 +26,7 @@ def _main():
     os.chdir(outdir)
 
     beamdirs = find_vcraft(datadir, beam=args.beam)
+    npol = len(beamdirs)
 
     # select specific card/FPGA to process and process it
     freqlabels = []
@@ -42,8 +43,10 @@ def _main():
     if os.path.exists("../../.bat0"):
         os.system("cp ../../.bat0 .")
 
-    torun = f"{args.dir}/vcraft2obs.py --dir={args.dir} " \
+    torun = (
+        f"{args.dir}/vcraft2obs.py --dir={args.dir} "
         "--startmjd={str(args.startmjd)}"
+    )
     if args.keep:
         torun += " -k"
     if args.ra is not None:
@@ -80,10 +83,10 @@ def _main():
         torun += " --calconly"
     beamname = os.path.basename(beamdirs[0])
     torun += f" --fpga {freqlabel}"
-    torun += f'"{timestep}/ak*/{beamname}/*{freqlabel}*vcraft"'
+    torun += f'"{datadir}/ak*/{beamname}/*{freqlabel}*vcraft"'
     if npol == 2:
         beamname = os.path.basename(beamdirs[1])
-        torun += f' "{timestep}/ak*/{beamname}/*{freqlabel}*vcraft"'
+        torun += f' "{datadir}/ak*/{beamname}/*{freqlabel}*vcraft"'
 
     print(torun)
     ret = runCommand(torun, "vcraft2obs.log")
@@ -151,124 +154,162 @@ def get_args() -> argparse.Namespace:
     :rtype: :class:`argparse.Namespace`
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--timestep",
-                        help="Timestep (the directory name to process)",
-                        required=True,
-                        )
-    parser.add_argument("--name",
-                        default="CRAFT",
-                        help="Base name for the output fits files",
-                        )
+    parser.add_argument(
+        "-t",
+        "--timestep",
+        help="Timestep (the directory name to process)",
+        required=True,
+    )
+    parser.add_argument(
+        "--name",
+        default="CRAFT",
+        help="Base name for the output fits files",
+    )
     parser.add_argument("-r", "--ra", help="Force RA value")
-    parser.add_argument("-d", "--dec",
-                        help="Force Dec value: use no space if declination is "
-                             "negative, i.e., -d-63:20:23.3",
-                        )
-    parser.add_argument("-b", "--bits",
-                        type=int,
-                        default=1,
-                        help="Number of bits. Default 1",
-                        )
-    parser.add_argument("-i", "--integration",
-                        type=float,
-                        help="Correlation integration time",
-                        )
-    parser.add_argument("-n", "--nchan",
-                        type=int,
-                        help="Number of spectral channels",
-                        )
-    parser.add_argument("--forceFFT",
-                        default=False,
-                        action="store_true",
-                        help="Force FFT size to equal number of channels (don't increase to "
-                        "128)",
-                        )
-    parser.add_argument("-f", "--fcm",
-                        default="fcm.txt",
-                        help="Name of the fcm file",
-                        required=True,
-                        )
-    parser.add_argument("-p", "--polyco",
-                        help="Bin config file for pulsar gating",
-                        required=True
-                        )
-    parser.add_argument("-c", "--correctfpgadelays",
-                        default=False,
-                        action="store_true",
-                        help="Figure out and correct 7 microsec FPGA delays",
-                        )
-    parser.add_argument("-S", "--suppress",
-                        default=False,
-                        action="store_true",
-                        help="Don't create FITS file",
-                        )
-    parser.add_argument("-B", "--beam",
-                        help="Correlate a specific beam: blank means both"
-                        )
-    parser.add_argument("--card",
-                        default="",
-                        help="Correlate only a specific card; blank means all",
-                        )
-    parser.add_argument("-k", "--keep",
-                        default=False,
-                        action="store_true",
-                        help="Keep existing codif files",
-                        )
-    parser.add_argument("-s", "--snoopylog",
-                        help="Snoopy log file, default blank, if not default "
-                             "will use this to correlate on-pulse",
-                        required=True,
-                        )
-    parser.add_argument("--slurm",
-                        default=False,
-                        action="store_true",
-                        help="Use slurm batch jobs rather than running locally",
-                        )
-    parser.add_argument("--ts",
-                        default=0,
-                        type=int,
-                        help="Use taskspooler to run CRAFTConverter, with N "
-                             "parallel tasks",
-                        )
-    parser.add_argument("--gstar",
-                        default=False,
-                        action="store_true",
-                        help="Set if using gstar for correlation",
-                        )
-    parser.add_argument("--large",
-                        default=False,
-                        action="store_true",
-                        help="Set if 32 nodes, 384 tasks are required (i.e., "
-                             "23GB memory needed per task; else 16 nodes, 192 "
-                             "tasks will be used for 11.5GB per task",
-                        )
-    parser.add_argument("--numskylakenodes",
-                        default=1,
-                        type=int,
-                        help="Use 32x this many CPUs",
-                        )
+    parser.add_argument(
+        "-d",
+        "--dec",
+        help="Force Dec value: use no space if declination is "
+        "negative, i.e., -d-63:20:23.3",
+    )
+    parser.add_argument(
+        "-b",
+        "--bits",
+        type=int,
+        default=1,
+        help="Number of bits. Default 1",
+    )
+    parser.add_argument(
+        "-i",
+        "--integration",
+        type=float,
+        help="Correlation integration time",
+    )
+    parser.add_argument(
+        "-n",
+        "--nchan",
+        type=int,
+        help="Number of spectral channels",
+    )
+    parser.add_argument(
+        "--forceFFT",
+        default=False,
+        action="store_true",
+        help="Force FFT size to equal number of channels (don't increase to "
+        "128)",
+    )
+    parser.add_argument(
+        "-f",
+        "--fcm",
+        default="fcm.txt",
+        help="Name of the fcm file",
+        required=True,
+    )
+    parser.add_argument(
+        "-p",
+        "--polyco",
+        help="Bin config file for pulsar gating",
+        required=True,
+    )
+    parser.add_argument(
+        "-c",
+        "--correctfpgadelays",
+        default=False,
+        action="store_true",
+        help="Figure out and correct 7 microsec FPGA delays",
+    )
+    parser.add_argument(
+        "-S",
+        "--suppress",
+        default=False,
+        action="store_true",
+        help="Don't create FITS file",
+    )
+    parser.add_argument(
+        "-B", "--beam", help="Correlate a specific beam: blank means both"
+    )
+    parser.add_argument(
+        "--card",
+        default="",
+        help="Correlate only a specific card; blank means all",
+    )
+    parser.add_argument(
+        "-k",
+        "--keep",
+        default=False,
+        action="store_true",
+        help="Keep existing codif files",
+    )
+    parser.add_argument(
+        "-s",
+        "--snoopylog",
+        help="Snoopy log file, default blank, if not default "
+        "will use this to correlate on-pulse",
+        required=True,
+    )
+    parser.add_argument(
+        "--slurm",
+        default=False,
+        action="store_true",
+        help="Use slurm batch jobs rather than running locally",
+    )
+    parser.add_argument(
+        "--ts",
+        default=0,
+        type=int,
+        help="Use taskspooler to run CRAFTConverter, with N " "parallel tasks",
+    )
+    parser.add_argument(
+        "--gstar",
+        default=False,
+        action="store_true",
+        help="Set if using gstar for correlation",
+    )
+    parser.add_argument(
+        "--large",
+        default=False,
+        action="store_true",
+        help="Set if 32 nodes, 384 tasks are required (i.e., "
+        "23GB memory needed per task; else 16 nodes, 192 "
+        "tasks will be used for 11.5GB per task",
+    )
+    parser.add_argument(
+        "--numskylakenodes",
+        default=1,
+        type=int,
+        help="Use 32x this many CPUs",
+    )
     parser.add_argument("-o", help="Output directory for data")
     parser.add_argument("--freqlabel", help="Freqlabel to process", type=str)
-    parser.add_argument("--calconly",
-                        default=False,
-                        action="store_true",
-                        help="Stop after creating .calc file",
-                        )
+    parser.add_argument(
+        "--calconly",
+        default=False,
+        action="store_true",
+        help="Stop after creating .calc file",
+    )
     parser.add_argument("--dir", help="Directory of local difx files")
-    parser.add_argument("--startmjd",
-                        default=-1,
-                        type=float,
-                        help="Force a particular start MJD without searching "
-                             "files for one.",
-                        )
-    return parser.parse_args()
+    parser.add_argument(
+        "--startmjd",
+        default=-1,
+        type=float,
+        help="Force a particular start MJD without searching "
+        "files for one.",
+    )
+    args = parser.parse_args()
+    verify_args(args, parser)
+    return args
 
 
-def verify_args(args: argparse.Namespace) -> None:
+def verify_args(
+    args: argparse.Namespace,
+    parser: argparse.ArgumentParser,
+) -> None:
     """Verify that provided command line arguments are valid
 
     :param args: Command line arguments
     :type args: :class:`argparse.Namespace`
+    :param parser: Parser object used to parse arguments
+    :type parser: :class:`argparse.ArgumentParser`
     """
 
     # Check that sensible options were given for the queue destination
@@ -296,7 +337,7 @@ def verify_args(args: argparse.Namespace) -> None:
         parser.error(f"{args.fcm} doesn't exist")
 
     if not os.path.exists(args.polyco):
-        parser.error(f"binconfig file {polyco} does not exist")
+        parser.error(f"binconfig file {args.polyco} does not exist")
 
 
 def get_nbins(polyco: str) -> int:
@@ -320,30 +361,45 @@ def get_nbins(polyco: str) -> int:
     return nbins
 
 
-def find_vcraft(datadir: str, beam: str = None) -> list[str]:
-    """Find the vcraft files to be processed
+def find_vcraft(datadir: str, beam: str = None, card: str = "") -> "list[str]":
+    """Determine beam directories containing data to be processed.
+
+    Searches within the provided datadir for vcraft files. If none are
+    found, exit. If they are found, return a list of paths to the beam
+    subdirectories for the first antenna containing data.
 
     :param datadir: Directory containing the per-antenna directories
     :type datadir: str
     :param beam: Beam subdirectory to return. If None, both beams are
         returned. [Default = None]
+    :type beam: str, optional
+    :param card: If given, return files for only a specific card.
+        [Default = all]
+    :type card: str, optional
     :return: List of paths to beam directories to process
     :rtype: list[str]
     """
     examplefiles = []
-    antennadirs = sorted(glob.glob(f"{timestep}/ak*"))
 
+    # find all antenna subdirectories
+    antennadirs = sorted(glob.glob(f"{datadir}/ak*"))
+
+    """
+    For each antenna subdir, get the beam subdir(s) and look for vcraft
+    files inside of them, stopping once we've verified that any vcraft
+    files exist.
+    """
     for a in antennadirs:
-        if args.beam is None:
+        if beam is None:
             beamdirs = sorted(glob.glob(f"{a}/*"))
         else:
-            beamdirs = [f"{a}/{args.beam}"]
-            if not os.path.exists(f"{a}/{args.beam}"):
-                print(f"{a}/{args.beam} doesn't exist, aborting")
+            beamdirs = [f"{a}/{beam}"]
+            if not os.path.exists(f"{a}/{beam}"):
+                print(f"{a}/{beam} doesn't exist, aborting")
                 sys.exit()
 
         for b in beamdirs:
-            vcraftfiles = glob.glob(f"{b}/*[ac]{args.card}*vcraft")
+            vcraftfiles = glob.glob(f"{b}/*[ac]{card}*vcraft")
 
             if len(vcraftfiles) > 0:
                 examplefiles = sorted(vcraftfiles)
