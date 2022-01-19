cd34d4aec638714a3b4e1581229b4a2fea44f4a9
d-r-scott <danica.scott@postgrad.curtin.edu.au>
2022-01-19 13:25:57 +0800

    ✨ Add bare minimum to be able to install as a package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="craco-postproc",
+    version="0.1",
+    description="Post-processing pipelines for CRACO FRB voltages",
+    author="Danica Scott",
+    author_email="danica.scott@postgrad.curtin.edu.au",
+    py_modules=["craco_postproc"],
+)
