bbc2a7cb1ad2c4a994afeb2c15a1bda9b151893d
d-r-scott <danica.scott@postgrad.curtin.edu.au>
2022-01-24 10:17:51 +0800

    Absorb deripple generation in to deripple.py

--- craco_postproc/beamform/deripple.py
+++ craco_postproc/beamform/deripple.py
@@ -59,8 +59,6 @@ def deripple(FFFF, coeff_dir, fftLength=1048576, bw=336):
     # de-ripple coefficients
     dr_c_file = coeff_dir + "/deripple_res6_nfft" + str(fftLength) + ".npy"
     if os.path.exists(dr_c_file) == False:
-        from generate_deripple import generate_deripple
-
         print("No derippling coefficient found. Generating one...")
         generate_deripple(fftLength, 6, coeff_dir)
     print(f"loading {dr_c_file}")
@@ -86,6 +84,28 @@ def deripple(FFFF, coeff_dir, fftLength=1048576, bw=336):
             )
 
     return FFFF
+
+
+def generate_deripple(nfft, res, dir):
+    N = 1536
+    OS_De = 27.0
+    OS_Nu = 32.0
+    dr = dir + "/ADE_R6_OSFIR.mat"
+    h = io.loadmat(dr)["c"][0]
+    passbandLength = int(((nfft / 2) * OS_De) / OS_Nu)
+    multiple = int(1536 / res)
+
+    # Modifications: pre-pad h with zeros
+    #                use scipy's fft instead of numpy's, as np's was segfaulting for huuuuge multiple*passbandLength*2
+    h_0 = np.zeros(multiple * passbandLength * 2)
+    h_0[: h.shape[0]] = h
+    temp = abs(fft.fft(h_0))  # ,multiple*passbandLength*2))
+    print(
+        "saving {}".format(
+            dir + "/deripple_res" + str(res) + "_nfft" + str(nfft)
+        )
+    )
+    np.save(dir + "/deripple_res" + str(res) + "_nfft" + str(nfft), temp)
 
 
 def save(f, fname):
