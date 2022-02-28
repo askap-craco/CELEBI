beff9b1882818dc59954d762db8d40b5cc315b2f
Danica Scott <danica.scott@postgrad.curtin.edu.au>
2022-02-28 05:02:37 +0000

    🚧 Give more helpful error message for invalid fitspath

--- craco_postproc/localise/calibrateFRB.py
+++ craco_postproc/localise/calibrateFRB.py
@@ -647,7 +647,7 @@ def out_fnames(fitspath: str) -> "tuple[str, str]":
     """
     assert (
         fitspath[:-5] == ".fits" or fitspath[:-7] == ".uvfits"
-    ), "fits filename must end in either .fits or .uvfits!"
+    ), f"fits filename must end in either .fits or .uvfits! This one is {fitspath}"
 
     fitsbase = fitspath.split("/")[-1]
     if ".uvfits" in fitspath:
