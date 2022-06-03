25d1fc9d0c36e1874e37de75aa5903bf353126d5
Danica Scott <danica.scott@postgrad.curtin.edu.au>
2022-06-03 07:02:49 +0000

    ✨ Upgrade dynspec plotting

--- craco_postproc/beamform/dynspecs.py
+++ craco_postproc/beamform/dynspecs.py
@@ -1,6 +1,7 @@
 import sys
 import time
 
+import matplotlib.gridspec as gs
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -213,76 +214,101 @@ def reduce(a, n, axis=0):
 
 
 def plot_IQUV_dts(
-    ds,
+    ds_list,
     f0,
     facs=[10, 30, 100, 300],
     time_range=100,
-    peaks=None,
-    fig=None,
-    axs=None,
-    title=True,
-    xlabel=True,
-    xticks=True,
+    labels=None,
+    fname=None,
 ):
-    new_peaks = []
+    fig = plt.figure(figsize=(2.5 * len(ds_list), 2.5 * len(facs)))
+    spec = gs.GridSpec(nrows=len(ds_list) + 1, ncols=3 * len(facs) + 1)
 
-    if axs is None or fig is None:
-        fig, axs = plt.subplots(nrows=1, ncols=len(facs), figsize=(12, 6))
+    peaks = []
 
-    for i, dt in enumerate(facs):
-        ds_red = reduce(ds, dt, axis=1)
-        peak = peaks[i] if peaks else np.argmax(np.sum(ds_red, axis=0))
-        new_peaks.append(peak)
-        plot_ds = ds_red[:, peak - time_range : peak + time_range]
-        ax = axs.flatten()[i]
-        ax.imshow(
-            plot_ds,
-            aspect="auto",
-            interpolation="none",
-            extent=(
-                -time_range * dt / 1e3,
-                time_range * dt / 1e3,
-                f0 - 336 / 2,
-                f0 + 336 / 2,
-            ),
-        )
+    prof_axs = [fig.add_subplot(spec[0, 3 * j : 3 * (j + 1)]) for j in range(len(facs))]
 
-        if xlabel:
-            ax.set_xlabel("Time (ms)")
-        
-        if not xticks:
-            ax.set_xticks([])
+    for i, ds in enumerate(ds_list):
+        for j, dt in enumerate(facs):
+            ds_red = reduce(ds, dt, axis=1)
+            if len(peaks) == j:
+                # first time through timescales
+                peaks.append(np.argmax(np.sum(ds_red, axis=0)))
 
-        if title:
-            ax.set_title(f"dt = {dt} us")
+            peak = peaks[j]
 
-        if i == 0:
-            ax.set_ylabel("Frequency (MHz)")
-        else:
-            ax.set_yticks([])
+            plot_ds = ds_red[:, peak - time_range : peak + time_range]
+            plot_ts = np.sum(plot_ds, axis=0)
+            t = np.linspace(
+                -time_range * dt / 1e3, time_range * dt / 1e3, plot_ts.shape[0]
+            )
 
-    return (fig, axs, new_peaks)
+            prof_ax = prof_axs[j]
+            ds_ax = fig.add_subplot(spec[i + 1, 3 * j : 3 * (j + 1)])
+
+            prof_ax.step(t, plot_ts, lw=1, label=labels[i])
+            prof_ax.set_xlim(t[0], t[-1])
+            ds_ax.imshow(
+                plot_ds,
+                aspect="auto",
+                interpolation="none",
+                extent=(
+                    -time_range * dt / 1e3,
+                    time_range * dt / 1e3,
+                    f0 - 336 / 2,
+                    f0 + 336 / 2,
+                ),
+            )
+
+            prof_ax.set_xticks([])
+            if i == 0:
+                # first row
+                prof_ax.set_title(f"dt = {dt} us")
+                ds_ax.set_xticks([])
+            elif i == len(ds_list) - 1:
+                # last row
+                ds_ax.set_xlabel("Time (ms)")
+            else:
+                ds_ax.set_xticks([])
+
+            prof_ax.set_yticks([])
+            if j == 0:
+                # first column
+                ds_ax.set_ylabel("Frequency (MHz)")
+            else:
+                ds_ax.set_yticks([])
+
+        # spectrum column
+        plot_spec = ds_red[:, peak]
+        freqs = np.linspace(f0 - 336 / 2, f0 + 336 / 2, plot_spec.shape[0])
+
+        spec_ax = fig.add_subplot(spec[i + 1, -1])
+        spec_ax.step(plot_spec, freqs[::-1], c="k", lw=1)
+        spec_ax.axvline(0, c="k", lw=1, ls="--")
+
+        spec_ax.set_yticks([])
+        spec_ax.set_xticks([])
+
+        spec_ax.set_ylim(freqs[0], freqs[-1])
+
+        spec_ax.yaxis.set_label_position("right")
+        spec_ax.set_ylabel(labels[i], rotation=0, fontsize=20, labelpad=20)
+
+    prof_ax.legend(bbox_to_anchor=(1, 0.5), loc="center left")
+
+    plt.subplots_adjust(
+        wspace=0, hspace=0, left=0.075, right=0.95, top=0.975, bottom=0.05
+    )
+
+    if fname:
+        plt.savefig(fname)
 
 
 def plot(args, stokes_fnames):
     stks = [np.load(f, mmap_mode="r") for f in stokes_fnames]
 
     # IQUV over four timescales
-    fig, axs = plt.subplots(nrows=4, ncols=4, figsize=(10, 10))
-    for i, ds in enumerate(stks):
-        fig, a, peaks = plot_IQUV_dts(
-            ds,
-            args.f,
-            time_range=50,
-            fig=fig,
-            axs=axs[i],
-            xlabel=True if i == 3 else False,
-            xticks=True if i == 3 else False,
-            title=True if i == 0 else False,
-            peaks=None if i == 0 else peaks,
-        )
-    plt.tight_layout()
-    plt.savefig(f"{args.label}_IQUV_dts.png")
+    plot_IQUV_dts(stks, args.f0, labels=["I", "Q", "U", "V"], fname=f"{args.label}_IQUV_dts.png")
 
 if __name__ == "__main__":
     _main()
