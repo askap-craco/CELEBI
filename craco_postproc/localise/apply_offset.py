ab230d6c0ead65b318a7224f1cb8e95a30199ba5
Danica Scott <danica.scott@postgrad.curtin.edu.au>
2022-09-30 11:50:57 +1000

    Fix wrong method of offset application

--- craco_postproc/localise/apply_offset.py
+++ craco_postproc/localise/apply_offset.py
@@ -45,24 +45,22 @@ FRB = Coord(args.frb)
 offset_ra, offset_ra_err, offset_dec, offset_dec_err = np.loadtxt(args.offset)
 
 frb_pos = sc(FRB.ra_hms, FRB.dec_dms, unit=("hourangle", "deg"))
-frb_err = sc(FRB.ra_err, FRB.dec_err, unit="arcsecond")
 
-offset = sc(offset_ra, offset_dec, unit="arcsecond")
-offset_err = sc(offset_ra_err, offset_dec_err, unit="arcsecond")
-
-# sum positions
-final_pos = sc(frb_pos.ra + offset.ra, frb_pos.dec + offset.dec)
-final_err = sc(
-    (frb_err.ra ** 2 + offset_err.ra ** 2) ** 0.5,
-    (frb_err.dec ** 2 + offset_err.dec ** 2) ** 0.5,
+final_pos = sc(
+    frb_pos.ra + offset_ra*un.arcsecond/np.cos(frb_pos.dec.to(un.rad)),
+    frb_pos.dec + offset_dec*un.arcsecond
 )
+
+final_err_ra = (FRB.ra_err ** 2 + offset_ra_err ** 2) ** 0.5
+final_err_dec = (FRB.dec_err ** 2 + offset_dec_err ** 2) ** 0.5
+
 
 print("FINAL FRB POSITION")
 print(
     f'RA:\t{final_pos.ra.to_string("hour")}'
-    f'\t+-{final_err.ra.to_string("arcsecond")}'
+    f'\t+-{final_err_ra}'
 )
 print(
     f'Dec:\t{final_pos.dec.to_string("deg")}'
-    f'\t+-{final_err.dec.to_string("arcsecond")}'
+    f'\t+-{final_err_dec}'
 )
