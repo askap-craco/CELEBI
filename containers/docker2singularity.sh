b93c74a967ddc6e5b38b59b6b2ff987aba174568
PaulHancock <paul.hancock@curtin.edu.au>
2024-08-12 15:00:51 +0800

    change name of default image file

--- containers/docker2singularity.sh
+++ containers/docker2singularity.sh
@@ -2,4 +2,4 @@ docker run -v /var/run/docker.sock:/var/run/docker.sock \
 -v .:/output \
 --privileged -t --rm \
 quay.io/singularity/docker2singularity \
--name cracofunew_latest cracofunew:latest
+-name cracofunew cracofunew:latest
