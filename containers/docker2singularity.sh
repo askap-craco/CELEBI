6e2ed060a64028e91e566d3326ba1923469eb8ad
PaulHancock <paul.hancock@curtin.edu.au>
2024-08-15 14:49:45 +0800

    use full year in image name

--- containers/docker2singularity.sh
+++ containers/docker2singularity.sh
@@ -2,4 +2,4 @@ docker run -v /var/run/docker.sock:/var/run/docker.sock \
 -v ./out:/output \
 --privileged -t --rm \
 quay.io/singularity/docker2singularity \
---name cracofunew-$(date +%y-%m-%d) cracofunew:latest
+--name cracofunew-$(date +%Y-%m-%d) cracofunew:latest
